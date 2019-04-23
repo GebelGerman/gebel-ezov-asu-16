1417002abc5b09506eb1c08aebef4687db43caa2
gebelgerman <gebel.german@yahoo.com>
2019-04-23 18:32:01 +0500

    'parser_demo'

---
+++ html/parser/filmsparser.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+
+url = "https://videomore.ru/films/komediya"
+
+
+def get_html(url):
+    req = Request(url)
+    html = urlopen(req).read()
+    return html
+
+def parse(html):
+    soup = BeautifulSoup(html, "html.parser")
+    table = soup.find(class_="project_vertical_list")
+    films = []
+    for row in table.find_all(class_="project_vertical_item"):
+        name = row.find(class_='pvi-title')
+        description = row.find(class_='pvi-description')
+        text = str(description.text)
+        a = text.split('\n')
+        films.append({
+            'Название': name.text,
+            'Жанр': a[1],
+            'Год': a[2],
+            'Страна': a[3]
+        })
+    return films
+
+
+def main():
+    films = parse(get_html(url))
+    for film in films:
+        print('----------')
+        print(film)
+        
+    
+
+if __name__ == "__main__":
+    main()
